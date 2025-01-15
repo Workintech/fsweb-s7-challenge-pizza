88328dcaa481da105bf295fdf17b2b1c6260cd7c
MrlKsglu <114409570+MrlKsglu@users.noreply.github.com>
2025-01-15 20:29:39 +0300

    Update App.jsx
    ana sayfa css düzenlemesi

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+      <body>
+
+        <div>
+          <h1>Teknolojik Yemekler</h1>
+          <h2>KOD ACIKTIRIR</h2>
+          <h2>PİZZA, DOYURUR</h2>
+          <button class="custom-button">ACIKTIM</button>
+
+        </div>
+        
+          
+
+        
+      </body>
     </>
   )
 }
